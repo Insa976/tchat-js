e1fd04f26db16c097b813dec0c34b53d1e6b887f
Insa <inssa.insa976@outlook.fr>
2020-03-05 22:28:26 +0100

    delete

--- server/methode_get_post_ajax/count_msg.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once "../../server.php";
-
-	$nb = $Message->getMessageCount($_GET['iduser1'], $_GET['iduser2']);
-	$nbJson = array('nbMsge' => $nb);
-
-	echo json_encode($nbJson);
-?>
